b519ab94f2123cb6cf1198670e0cf11ab524a594
Chiara Zambelli <chiara.zambelli@smeup.com>
2018-11-23 08:54:51 +0100

    it: test of junit report

--- tests/e2e/specs/app1T.js
+++ tests/e2e/specs/app1T.js
@@ -2,6 +2,6 @@
 describe("Showcase sample test 1", () => {
   it("Visits the app root url 1", () => {
     cy.visit("/");
-    cy.get('#app').children().first().should('contain', 'Test');
+    cy.get('#app').children().first().should('contain', 'TestX');
   });
 });
